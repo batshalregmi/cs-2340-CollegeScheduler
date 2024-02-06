20e3d6c0918b0b7ae371f10f7057cfe9ddef565b
patelshubh1022 <patelshubh1022@gmail.com>
2024-02-06 16:27:59 -0500

    this shit goated

--- app/src/main/java/com/example/collegeschedulerproject/ExamInfo.java
+++ app/src/main/java/com/example/collegeschedulerproject/ExamInfo.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import java.lang.*;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.icu.util.Calendar;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -40,6 +42,7 @@ public class ExamInfo extends AppCompatActivity {
         addButton = findViewById(R.id.exam_add_button);
         txtTime = findViewById(R.id.btnTimePicker);
         textBox = findViewById(R.id.exam_edit_text);
+        txtDate = findViewById(R.id.btnDatePicker);
         ExamInfoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ExamInfoList);
 
         ExamInfoListView.setAdapter(ExamInfoAdapter);
@@ -47,9 +50,14 @@ public class ExamInfo extends AppCompatActivity {
             if (textBox.getText().toString().equals("")) {
                 Toast.makeText(this, "Not enough information added!", Toast.LENGTH_SHORT).show();
             } else {
-                ExamInfoList.add(textBox.getText().toString());
+                ExamInfoList.add("Exam Name: " + textBox.getText().toString() + "\nExam Date: " + txtDate.getText().toString() + "\nTime: " +  txtTime.getText().toString());
                 ExamInfoAdapter.notifyDataSetChanged();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
                 textBox.getText().clear();
+                txtDate.getText().clear();
+                txtTime.getText().clear();
+
             }
         });
         ExamInfoListView.setOnItemLongClickListener((parent, view, position, id) -> {
@@ -59,7 +67,11 @@ public class ExamInfo extends AppCompatActivity {
         });
         ExamInfoListView.setOnItemClickListener((parent, view, position, id) -> {
             showEditDialog(position);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
         });
+        txtTime.setFocusable(false);
+        txtDate.setFocusable(false);
         txtTime.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -78,24 +90,59 @@ public class ExamInfo extends AppCompatActivity {
                                 public void onTimeSet(TimePicker view, int hourOfDay,
                                                       int minute) {
 
-                                    txtTime.setText(hourOfDay + ":" + minute);
+                                    //convert to 12hr time
+                                    if (hourOfDay > 12) {
+                                        hourOfDay = hourOfDay - 12;
+                                        txtTime.setText(hourOfDay + ":" + minute + " PM");
+                                    } else if (hourOfDay == 0) {
+                                        hourOfDay = 12;
+                                        txtTime.setText(hourOfDay + ":" + minute + " AM");
+                                    } else if (hourOfDay == 12) {
+                                        txtTime.setText(hourOfDay + ":" + minute + " PM");
+                                    } else {
+                                        txtTime.setText(hourOfDay + ":" + minute + " AM");
+                                    }
                                 }
                             }, mHour, mMinute, false);
                     timePickerDialog.show();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
                 }
+            }
+        });
+        txtDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ExamInfo.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                txtDate.setText((monthOfYear + 1) + "-" + dayOfMonth +  "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
             }
         });
         theFilter.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 (ExamInfo.this).ExamInfoAdapter.getFilter().filter(s);
             }
-
             @Override
             public void afterTextChanged(Editable s) {
 
@@ -119,11 +166,17 @@ public class ExamInfo extends AppCompatActivity {
             ExamInfoList.set(position, editText.getText().toString());
             ExamInfoAdapter.notifyDataSetChanged();
             dialog.dismiss();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
         });
 
         builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
 
         builder.show();
+        InputMethodManager imm2 = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm2.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
 
 
